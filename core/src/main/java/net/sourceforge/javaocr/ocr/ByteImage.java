1642c4998019a38f2bc9d7860c96a7739de301bf
Konstantin Pribluda <ko5tik>
2010-10-16 14:21:39 +0000

    byte image here

---
+++ core/src/main/java/net/sourceforge/javaocr/ocr/ByteImage.java
@@ -0,0 +1,32 @@
+package net.sourceforge.javaocr.ocr;
+
+/**
+ * image backed by byte array
+ */
+public class ByteImage extends AbstractLinearImage {
+    byte[] image;
+
+
+    public ByteImage(int width, int height) {
+        this(new byte[width * height], width, height);
+    }
+
+    public ByteImage(byte[] image, int width, int height) {
+        this(image, width, height, 0, 0, width, height);
+    }
+
+    public ByteImage(byte[] image, int arrayWidth, int arrayHeight, int originX, int originY, int width, int height) {
+        super(arrayWidth, arrayHeight, originX, originY, width, height);
+        this.image = image;
+    }
+
+    @Override
+    public int get() {
+       return image[currentIndex];
+    }
+
+    @Override
+    public void put(int value) {
+        image[currentIndex] = (byte) value;
+    }
+}
