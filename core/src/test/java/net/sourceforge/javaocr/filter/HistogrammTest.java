7cb3f518a5aaddd78f739a72588db903cf50bc79
Konstantin Pribluda <ko5tik>
2010-10-18 18:53:55 +0000

    improving test coverage for filters and byte image

--- core/src/test/java/net/sourceforge/javaocr/filter/HistogrammTest.java
+++ core/src/test/java/net/sourceforge/javaocr/filter/HistogrammTest.java
@@ -7,7 +7,7 @@ import net.sourceforge.javaocr.ocr.PixelImage;
  * assure proper functionlaity of histogramm filter
  */
 public class HistogrammTest extends TestCase {
-    int[] values = new int[]{0, 0, 1, 1, 2, 2, 255, 255, 254, 254, 253, 253};
+    int[] values = new int[]{0, 1, 1, 2, 2, 2, 253, 253, 253, 254, 254, 255};
 
     /**
      * histogramm values shall be collected properly
@@ -19,28 +19,58 @@ public class HistogrammTest extends TestCase {
         filter.process(image);
 
         assertEquals(12, filter.getTotalCount());
-        assertEquals(2, filter.getHistogramm()[0]);
+        assertEquals(1, filter.getHistogramm()[0]);
         assertEquals(2, filter.getHistogramm()[1]);
-        assertEquals(2, filter.getHistogramm()[2]);
+        assertEquals(3, filter.getHistogramm()[2]);
         for (int i = 3; i < 253; i++)
             assertEquals(0, filter.getHistogramm()[i]);
-        assertEquals(2, filter.getHistogramm()[253]);
+        assertEquals(3, filter.getHistogramm()[253]);
         assertEquals(2, filter.getHistogramm()[254]);
-        assertEquals(2, filter.getHistogramm()[255]);
+        assertEquals(1, filter.getHistogramm()[255]);
 
     }
 
     /**
      * shall perform folding properly
-     *    - clamp values on border to 0
-     *    - perform proper computations of values
+     * - clamp values on border to 0
+     * - perform proper computations of values
      */
     public void testFolding() {
-        int[] folder = new int[] { 1,0,1};
+        int[] folder = new int[]{1, 1, 1};
         PixelImage image = new PixelImage(values, 1, 12);
         HistogramFilter filter = new HistogramFilter();
         filter.process(image);
+        // fold it
+        filter.fold(folder);
+
+        //first one shall be 0   / clamped
+        assertEquals(0, filter.getHistogramm()[0]);
+
+        // (1 * 1 + 1 * 2 + 1 * 3)/3 -> 2
+        assertEquals(2, filter.getHistogramm()[1]);
+        // (1 * 2 + 1* 3+ 1 * 0)/3 -> 1
+        assertEquals(1, filter.getHistogramm()[2]);
+        // (1 * 3 + 1* 0+ 1 * 0)/3 -> 1
+        assertEquals(1, filter.getHistogramm()[3]);
+        for (int i = 4; i < 252; i++)
+            assertEquals(0, filter.getHistogramm()[i]);
+
+        // reverse
+        assertEquals(1, filter.getHistogramm()[252]);
+        assertEquals(1, filter.getHistogramm()[253]);
+        assertEquals(2, filter.getHistogramm()[254]);
+        assertEquals(0, filter.getHistogramm()[255]);
+    }
 
 
+    public void testEvenSizeProducesIAE() {
+        int[] folder = new int[]{1, 1, 1, 1};
+        HistogramFilter filter = new HistogramFilter();
+        try {
+            filter.fold(folder);
+            fail("no exception thrown on even golding array size");
+        } catch (IllegalArgumentException e) {
+            // thar's ok, we await this one
+        }
     }
 }
--- core/src/test/java/net/sourceforge/javaocr/ocr/ByteImageTest.java
+++ core/src/test/java/net/sourceforge/javaocr/ocr/ByteImageTest.java
@@ -3,8 +3,19 @@ package net.sourceforge.javaocr.ocr;
 import junit.framework.TestCase;
 
 /**
-* test capabilities of byte images
+ * test capabilities of byte images
  * TODO: imaplement testing of byte to int conversion,  shall be able to set and retrieve values from 0 to 255 properly, maybe bigger
  */
 public class ByteImageTest extends TestCase {
+
+    public void testValuesAreSetAndRetrievedProperlyAsUnsigned() {
+        ByteImage image = new ByteImage(1, 1);
+
+        image.setCurrentIndex(0, 0);
+        image.put(255);
+        assertEquals(255, image.get());
+
+        image.put(129);
+        assertEquals(129, image.get());
+    }
 }
