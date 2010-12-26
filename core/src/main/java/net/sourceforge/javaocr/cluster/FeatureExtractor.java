66edc55908a42ba2e08486c9d05c324bd3fc0b91
Konstantin Pribluda <ko5tik>
2010-12-26 19:02:38 +0000

    moving cluster analysis interfaces into core

---
+++ core/src/main/java/net/sourceforge/javaocr/cluster/FeatureExtractor.java
@@ -0,0 +1,27 @@
+package net.sourceforge.javaocr.cluster;
+
+import net.sourceforge.javaocr.Image;
+
+/**
+ * extract image features to be used in cluster analysis. concrete implementations will be
+ * provided by plugins.
+ */
+public interface FeatureExtractor {
+    /**
+     * not sure whether we really need this,  as some extractors may provide variable
+     * amount of features
+     * TODO: do we really need this ?
+     *
+     * @return size of produced feature vector
+     */
+    public int getSize();
+
+    /**
+     * extract image features
+     *
+     * @param image
+     * @return
+     */
+    public double[] extract(Image image);
+
+}
---
+++ core/src/main/java/net/sourceforge/javaocr/cluster/Metric.java
@@ -0,0 +1,16 @@
+package net.sourceforge.javaocr.cluster;
+
+/**
+ * metric computes "distance" between feature vector extracted from candidate image
+ * and data stored inside match.
+ */
+public interface Metric {
+
+    /**
+     * compute distance between metric and feature vectors. INdividual plugins will define concrete
+     * metric implementation ( euclidian, 
+     * @param features
+     * @return
+     */
+    double distance(double[] features);
+}
